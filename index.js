133e179aefb17d65290cbfa0f3784a793a870a31
mintusarker <sarkermintu707@gmail.com>
2023-03-05 01:55:23 +0600

    server

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const app = express()
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dl1tykd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productCollection = client.db('productName').collection('products')
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const result = await productCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/products', async (req, res) => {
+            const query = req.body;
+            const result = await productCollection.insertOne(query);
+            res.send(result);
+        });
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(filter);
+            res.send(result)
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
+
+
+
+app.get('/', async (req, res) => {
+    res.send('server running')
+})
+
+
+app.listen(port, () => console.log(`server running on ${port}`))
